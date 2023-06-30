b9c4cac400e2b18657e54fac753b0b3ac5b6dced
wrnlb <103810793+wrnlb666@users.noreply.github.com>
2023-06-30 18:07:18 -0400

    example for C string

---
+++ test4.c
@@ -0,0 +1,30 @@
+#include "src/list.h"
+
+int main( void )
+{
+    char* s1 = "Hello";
+    char* s2 = " ";
+    char* s3 = "World";
+    char* s4 = "!";
+    char* s5 = "\n";
+
+    char *s[] = { s3, s2, s1, s4, s5 };
+
+    char** arr = list_create_args( .type = LIST_STR );
+
+    arr = list_append( arr, s1 );
+    arr = list_append( arr, s2 );
+    arr = list_append( arr, s3 );
+    arr = list_append( arr, s4 );
+    arr = list_append( arr, s5 );
+    arr = list_inserts( arr, 0, 5, s );
+
+    for ( size_t i = 0; i < list_len( arr ); i++ )
+    {
+        printf( "%s", arr[i] );
+    }
+
+    list_destroy( arr );
+
+    return 0;
+}
